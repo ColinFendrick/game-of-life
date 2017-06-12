19d3911d78760a38cefd72a61cb4307b46be02d1
Colin Fendrick <colin.fendrick@gmail.com>
2017-06-12 09:10:52 -0400

    Removed react router, doing one page app

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Gameboard from './Gameboard'
-import Intro from './Intro'
 
 class App extends Component {
   render () {
-    return <Router>
-      <div>
-        <Route exact path='/' component={Intro} />
-        <Route exact path='/game' component={Gameboard} />
-      </div>
-    </Router>
+    return <div>
+      <Gameboard />
+    </div>
   }
 }
 
